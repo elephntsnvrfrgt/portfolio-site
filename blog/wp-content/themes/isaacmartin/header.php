78e411ea24c36e699b2f0669efc84f2d890cdfaf
imartin <imartin@imartin-mac.gateway.2wire.net>
2014-03-10 00:22:05 -0400

    stripping out wordpress junk, customizing post format
    i have no idea what i'm doing...

--- blog/wp-content/themes/isaacmartin/header.php
+++ blog/wp-content/themes/isaacmartin/header.php
@@ -28,22 +28,7 @@
 </head>
 <body <?php body_class(); ?>>
 <div id="wrapper" class="hfeed">
-	<header id="header" role="banner">
-		<section id="branding">
-			<div id="site-title">
-				<?php if ( ! is_singular() ) { echo '<h1>'; } ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( get_bloginfo( 'name' ), 'blankslate' ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( ! is_singular() ) { echo '</h1>'; } ?>
-			</div>
-			<div id="site-description">
-				<?php bloginfo( 'description' ); ?>
-			</div>
-		</section>
-		<nav id="menu" role="navigation">
-			<div id="search">
-				<?php get_search_form(); ?>
-			</div>
-			<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-		</nav>
-	</header>
+	
 
 	<header id="top" role="banner">
             <div class="block">
@@ -51,9 +36,13 @@
                 <a class="nav-btn" id="nav-open-btn" href="#nav"><span class="icon-list"></span></a>
             </div>
             <?php include '../nav.php'; ?>
-            <div id="search">
-				<?php get_search_form(); ?>
-			</div>
+           
         </header>
+        <header id="header" role="banner">
+		
+		<nav id="menu" role="navigation">
+			<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+		</nav>
+	</header>
 
 <div id="container">
